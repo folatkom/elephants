93494d69054aaf4b4c453b4eeb34ec29ab61fd87
Monika Folatko <folatko.monika@gmail.com>
2022-02-17 19:50:32 +0100

    Full algorithm

--- slonie.js
+++ slonie.js
@@ -9,9 +9,20 @@ let simpleArray =[];
 let pickedNum;
 let isFinished = false;
 
+//parameters
+let massArray = [];
+let minCycleWeight;
+let minGlobalWeight;
+let cyclesMasses = [];
+let simpleMassArray = [];
+let cycleMassSum;
+let method1;
+let method2;
+let result = 0;
+
 //convert to arrays
 let char = "";
-for (let i = 0; i <= orderIn.length; i++) {
+for (let i = 0; i < orderIn.length; i++) {
     if (i === orderIn.length-1){
         char += orderIn.charAt(i);
         inArray.push(char);
@@ -27,7 +38,7 @@ for (let i = 0; i <= orderIn.length; i++) {
         }        
     }  
 };
-for (let i = 0; i <= orderOut.length; i++) {
+for (let i = 0; i < orderOut.length; i++) {
     if (i === orderOut.length-1){
         char += orderOut.charAt(i);
         outArray.push(char);
@@ -39,6 +50,22 @@ for (let i = 0; i <= orderOut.length; i++) {
         }
         else if (orderOut.charAt(i) === " "){
             outArray.push(char);
+            char = "";
+        }        
+    }  
+};
+for (let i = 0; i < mass.length; i++) {
+    if (i === mass.length-1){
+        char += mass.charAt(i);
+        massArray.push(char);
+        char = "";
+    }
+    else {
+        if (mass.charAt(i) !== " ") {
+            char += mass.charAt(i);
+        }
+        else if (mass.charAt(i) === " "){
+            massArray.push(char);
             char = "";
         }        
     }  
@@ -56,7 +83,7 @@ const findNumbers = () => {
             pickedNum = spareArray[j];
             draftArray[j] = " ";
             if (pickedNum === spareNum) {
-                cyclesArray.push(simpleArray)           
+                cyclesArray.push(simpleArray);         
                 simpleArray = [];
                 return 0;
             }   
@@ -79,3 +106,24 @@ do {
     });
     index++;
 }while(!isFinished);
+
+//get parameters
+minGlobalWeight = Math.min(...massArray);
+for (let i = 0; i < cyclesArray.length; i++) {
+    simpleMassArray = [];
+    for (let j = 0; j < cyclesArray[i].length; j++) {
+        simpleMassArray.push(massArray[cyclesArray[i][j]-1]);
+    }
+    cyclesMasses.push(simpleMassArray);
+};
+for (let i = 0; i < cyclesMasses.length; i++) {
+    minCycleWeight = Math.min(...cyclesMasses[i]);
+    cycleMassSum = 0;
+    for (let j = 0; j < cyclesMasses[i].length; j++) {
+        cycleMassSum += Number(cyclesMasses[i][j]);
+    }
+    method1 = cycleMassSum + (cyclesMasses[i].length - 2) * minCycleWeight;
+    method2 = cycleMassSum + minCycleWeight + (cyclesMasses[i].length + 1) * minGlobalWeight;
+    result += Math.min(method1,method2);
+};
+console.log(result);
