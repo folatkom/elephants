120f6bd5d7e6063b5fcee77db159365cddd8988c
Monika Folatko <folatko.monika@gmail.com>
2022-02-17 18:24:35 +0100

    Refactor conversion to arrays

--- slonie.js
+++ slonie.js
@@ -1,5 +1,5 @@
 let n = 6;
-const ms = "2400 2000 1200 2400 1600 4000";
+const mass = "2400 2000 1200 2400 1600 4000";
 const orderIn = "1 4 5 3 6 2";
 const orderOut = "5 3 2 4 6 1";
 let inArray = [];
@@ -10,26 +10,51 @@ let pickedNum;
 let isFinished = false;
 
 //convert to arrays
-for (let i = 0; i < orderIn.length; i++) {
-    if (orderIn.charAt(i) !== " ") {
-        inArray.push(orderIn.charAt(i));
+let char = "";
+for (let i = 0; i <= orderIn.length; i++) {
+    if (i === orderIn.length-1){
+        char += orderIn.charAt(i);
+        inArray.push(char);
+        char = "";
     }
-}
-for (let i = 0; i < orderOut.length; i++) {
-    if (orderOut.charAt(i) !== " ") {
-        outArray.push(orderOut.charAt(i));
+    else {
+        if (orderIn.charAt(i) !== " ") {
+            char += orderIn.charAt(i);
+        }
+        else if (orderIn.charAt(i) === " "){
+            inArray.push(char);
+            char = "";
+        }        
+    }  
+};
+for (let i = 0; i <= orderOut.length; i++) {
+    if (i === orderOut.length-1){
+        char += orderOut.charAt(i);
+        outArray.push(char);
+        char = "";
     }
+    else {
+        if (orderOut.charAt(i) !== " ") {
+            char += orderOut.charAt(i);
+        }
+        else if (orderOut.charAt(i) === " "){
+            outArray.push(char);
+            char = "";
+        }        
+    }  
 };
 
 //divide into cycles
 let index = 0;
 let spareArray =[];
+let draftArray =[];
+draftArray = [...inArray]
 const findNumbers = () => {
     simpleArray.push(pickedNum);
     for(let j = 0; j < n; j++) {
         if(pickedNum === outArray[j]) {
             pickedNum = spareArray[j];
-            inArray[j] = " ";
+            draftArray[j] = " ";
             if (pickedNum === spareNum) {
                 cyclesArray.push(simpleArray)           
                 simpleArray = [];
@@ -42,14 +67,14 @@ const findNumbers = () => {
     } 
 };
 do {
-    if(inArray[index] !== " "){
-        pickedNum = inArray[index];
+    if(draftArray[index] !== " "){
+        pickedNum = draftArray[index];
         spareNum = pickedNum;
-        spareArray = [...inArray];
-        inArray[index] = " ";
+        spareArray = [...draftArray];
+        draftArray[index] = " ";
         findNumbers();
     };
-    isFinished = inArray.every((value) => {
+    isFinished = draftArray.every((value) => {
         return value === " ";
     });
     index++;
