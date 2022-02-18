e55c338c8c069384fcc84804ebc519985126d4ff
Monika Folatko <folatko.monika@gmail.com>
2022-02-18 10:11:58 +0100

    Refactor to avoid recursion

--- slonie.js
+++ slonie.js
@@ -43,56 +43,52 @@ rl.on('close', function () {
   let index = 0;
   let spareArray =[];
   let draftArray =[];
-  draftArray = [...inArray]
-  const findNumbers = () => {
-      simpleArray.push(pickedNum);
-      for(let j = 0; j < n; j++) {
-          if(pickedNum === outArray[j]) {
-              pickedNum = spareArray[j];
-              draftArray[j] = " ";
-              if (pickedNum === spareNum) {
-                  cyclesArray.push(simpleArray);         
-                  simpleArray = [];
-                  return 0;
-              }   
-              else {
-                  return findNumbers();
-              }         
-          }
-      } 
-  };
+  draftArray = [...inArray];
   do {
-      if(draftArray[index] !== " "){
-          pickedNum = draftArray[index];
-          spareNum = pickedNum;
-          spareArray = [...draftArray];
-          draftArray[index] = " ";
-          findNumbers();
-      };
-      isFinished = draftArray.every((value) => {
-          return value === " ";
-      });
-      index++;
+    if(draftArray[index] !== " "){
+      pickedNum = draftArray[index];
+      spareNum = pickedNum;
+      spareArray = [...draftArray];
+      draftArray[index] = " ";
+      do{
+        for(let j = 0; j < n; j++) {
+          if(pickedNum === outArray[j]) {
+            pickedNum = spareArray[j];
+            simpleArray.push(pickedNum);
+            draftArray[j] = " ";  
+              if(pickedNum === spareNum) {
+                cyclesArray.push(simpleArray);  
+                simpleArray = [];
+                break;
+              }       
+          }
+        }    
+      }while (pickedNum !== spareNum); 
+    };
+    isFinished = draftArray.every((value) => {
+      return value === " ";
+    });
+    index++;
   }while(!isFinished);
 
   //get parameters
   minGlobalWeight = Math.min(...massArray);
   for (let i = 0; i < cyclesArray.length; i++) {
-      simpleMassArray = [];
-      for (let j = 0; j < cyclesArray[i].length; j++) {
-          simpleMassArray.push(massArray[cyclesArray[i][j]-1]);
-      }
-      cyclesMasses.push(simpleMassArray);
+    simpleMassArray = [];
+    for (let j = 0; j < cyclesArray[i].length; j++) {
+      simpleMassArray.push(massArray[cyclesArray[i][j]-1]);
+    }
+    cyclesMasses.push(simpleMassArray);
   };
   for (let i = 0; i < cyclesMasses.length; i++) {
-      minCycleWeight = Math.min(...cyclesMasses[i]);
-      cycleMassSum = 0;
-      for (let j = 0; j < cyclesMasses[i].length; j++) {
-          cycleMassSum += Number(cyclesMasses[i][j]);
-      }
-      method1 = cycleMassSum + (cyclesMasses[i].length - 2) * minCycleWeight;
-      method2 = cycleMassSum + minCycleWeight + (cyclesMasses[i].length + 1) * minGlobalWeight;
-      result += Math.min(method1,method2);
+    minCycleWeight = Math.min(...cyclesMasses[i]);
+    cycleMassSum = 0;
+    for (let j = 0; j < cyclesMasses[i].length; j++) {
+      cycleMassSum += Number(cyclesMasses[i][j]);
+    }
+    method1 = cycleMassSum + (cyclesMasses[i].length - 2) * minCycleWeight;
+    method2 = cycleMassSum + minCycleWeight + (cyclesMasses[i].length + 1) * minGlobalWeight;
+    result += Math.min(method1,method2);
   };
   console.log(result);
 });
