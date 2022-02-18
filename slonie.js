f6902afb66625354fc902d0d4f137946885d5d08
Monika Folatko <folatko.monika@gmail.com>
2022-02-18 08:49:45 +0100

    Add standard input

--- slonie.js
+++ slonie.js
@@ -19,64 +19,80 @@ let method1;
 let method2;
 let result = 0;
 
-//convert to arrays
-inArray = orderIn.split(" ");
-outArray = orderOut.split(" ");
-massArray = mass.split(" ");
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+});
 
-//divide into cycles
-let index = 0;
-let spareArray =[];
-let draftArray =[];
-draftArray = [...inArray]
-const findNumbers = () => {
-    simpleArray.push(pickedNum);
-    for(let j = 0; j < n; j++) {
-        if(pickedNum === outArray[j]) {
-            pickedNum = spareArray[j];
-            draftArray[j] = " ";
-            if (pickedNum === spareNum) {
-                cyclesArray.push(simpleArray);         
-                simpleArray = [];
-                return 0;
-            }   
-            else {
-                return findNumbers();
-            }         
-        }
-    } 
-};
-do {
-    if(draftArray[index] !== " "){
-        pickedNum = draftArray[index];
-        spareNum = pickedNum;
-        spareArray = [...draftArray];
-        draftArray[index] = " ";
-        findNumbers();
-    };
-    isFinished = draftArray.every((value) => {
-        return value === " ";
-    });
-    index++;
-}while(!isFinished);
+global.dataArray = [];
+rl.on('line', function (line) {
+  global.dataArray.push(line);
+});
 
-//get parameters
-minGlobalWeight = Math.min(...massArray);
-for (let i = 0; i < cyclesArray.length; i++) {
-    simpleMassArray = [];
-    for (let j = 0; j < cyclesArray[i].length; j++) {
-        simpleMassArray.push(massArray[cyclesArray[i][j]-1]);
-    }
-    cyclesMasses.push(simpleMassArray);
-};
-for (let i = 0; i < cyclesMasses.length; i++) {
-    minCycleWeight = Math.min(...cyclesMasses[i]);
-    cycleMassSum = 0;
-    for (let j = 0; j < cyclesMasses[i].length; j++) {
-        cycleMassSum += Number(cyclesMasses[i][j]);
-    }
-    method1 = cycleMassSum + (cyclesMasses[i].length - 2) * minCycleWeight;
-    method2 = cycleMassSum + minCycleWeight + (cyclesMasses[i].length + 1) * minGlobalWeight;
-    result += Math.min(method1,method2);
-};
-console.log(result);
+rl.on('close', function () {
+  const n = global.dataArray[0];
+  const mass = global.dataArray[1];
+  const orderIn = global.dataArray[2];
+  const orderOut = global.dataArray[3];
+    
+  inArray = orderIn.split(" ");
+  outArray = orderOut.split(" ");
+  massArray = mass.split(" ");
+
+  //divide into cycles
+  let index = 0;
+  let spareArray =[];
+  let draftArray =[];
+  draftArray = [...inArray]
+  const findNumbers = () => {
+      simpleArray.push(pickedNum);
+      for(let j = 0; j < n; j++) {
+          if(pickedNum === outArray[j]) {
+              pickedNum = spareArray[j];
+              draftArray[j] = " ";
+              if (pickedNum === spareNum) {
+                  cyclesArray.push(simpleArray);         
+                  simpleArray = [];
+                  return 0;
+              }   
+              else {
+                  return findNumbers();
+              }         
+          }
+      } 
+  };
+  do {
+      if(draftArray[index] !== " "){
+          pickedNum = draftArray[index];
+          spareNum = pickedNum;
+          spareArray = [...draftArray];
+          draftArray[index] = " ";
+          findNumbers();
+      };
+      isFinished = draftArray.every((value) => {
+          return value === " ";
+      });
+      index++;
+  }while(!isFinished);
+
+  //get parameters
+  minGlobalWeight = Math.min(...massArray);
+  for (let i = 0; i < cyclesArray.length; i++) {
+      simpleMassArray = [];
+      for (let j = 0; j < cyclesArray[i].length; j++) {
+          simpleMassArray.push(massArray[cyclesArray[i][j]-1]);
+      }
+      cyclesMasses.push(simpleMassArray);
+  };
+  for (let i = 0; i < cyclesMasses.length; i++) {
+      minCycleWeight = Math.min(...cyclesMasses[i]);
+      cycleMassSum = 0;
+      for (let j = 0; j < cyclesMasses[i].length; j++) {
+          cycleMassSum += Number(cyclesMasses[i][j]);
+      }
+      method1 = cycleMassSum + (cyclesMasses[i].length - 2) * minCycleWeight;
+      method2 = cycleMassSum + minCycleWeight + (cyclesMasses[i].length + 1) * minGlobalWeight;
+      result += Math.min(method1,method2);
+  };
+  console.log(result);
+});
